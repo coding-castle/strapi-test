3dd8d15482a3f982c5bb66e15b89bcc5574db8a0
Patrick Goeler von Ravensburg <patrick.goeler.von.ravensburg@sap.com>
2021-04-25 21:21:37 +0200

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e0962115439e814dc3df55da298a00f1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ce741d82-1293-4a56-abf6-bbf9fbd63e36'
+};
