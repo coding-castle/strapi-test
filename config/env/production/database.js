5f6321c687c5158f53744b17739bfa694d9177b4
Patrick Goeler von Ravensburg <patrick.goeler.von.ravensburg@sap.com>
2021-04-25 21:29:00 +0200

    add postgres options

---
+++ config/env/production/database.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('PGHOST', 'localhost'),
+        port: env.int('PGPORT', 5432),
+        database: env('PGDATABASE', 'strapi'),
+        username: env('PGUSER', 'strapi'),
+        password: env('PGPASSWORD', 'strapi'),
+      },
+      options: {
+      },
+    },
+  },
+});
